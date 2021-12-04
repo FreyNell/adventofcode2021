a394f89df67c34f83fc51cb44b43545727006514
freynell <freybalaguera@gmail.com>
2021-12-04 02:41:34 +0000

    added benchmarking

--- day1/day1_test.go
+++ day1/day1_test.go
@@ -8,15 +8,7 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	var data []int
-	input := frey_utils.ReadInput("example_input")
-	for _, elem := range input {
-		num, err := strconv.Atoi(elem)
-		if err != nil {
-			fmt.Println("Error reading the data.")
-		}
-		data = append(data, num)
-	}
+	data := GetData()
 	want := 7
 	if got := Part1(data); got != want {
 		t.Errorf("Part1() = %q, want %q", got, want)
@@ -24,7 +16,28 @@ func TestPart1(t *testing.T) {
 }
 
 func TestPart2(t *testing.T) {
-	var data []int
+	data := GetData()
+	want := 5
+	if got := Part2(data); got != want {
+		t.Errorf("Part2() = %q, want %q", int(got), int(want))
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	data := GetData()
+	for i := 0; i < b.N; i++ {
+		Part1(data)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	data := GetData()
+	for i := 0; i < b.N; i++ {
+		Part2(data)
+	}
+}
+
+func GetData() (data []int) {
 	input := frey_utils.ReadInput("example_input")
 	for _, elem := range input {
 		num, err := strconv.Atoi(elem)
@@ -33,8 +46,5 @@ func TestPart2(t *testing.T) {
 		}
 		data = append(data, num)
 	}
-	want := 5
-	if got := Part2(data); got != want {
-		t.Errorf("Part2() = %q, want %q", int(got), int(want))
-	}
+	return
 }
